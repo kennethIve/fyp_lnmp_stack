3c15b39ac52af3a335543a5773678205293c3845
kenneth <kenneth96@outlook.com>
2020-06-17 01:03:52 +0800

    passport and personal key

--- app/laravel/resources/views/welcome.blade.php
+++ app/laravel/resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
+                <img src="" alt="">
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
