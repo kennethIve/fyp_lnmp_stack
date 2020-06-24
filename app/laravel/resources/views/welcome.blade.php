a547d7a9a48c314e751cf6d5054c97f693d5992d
kenneth <kenneth96@outlook.com>
2020-06-24 23:16:05 +0800

    please do on push

--- app/laravel/resources/views/welcome.blade.php
+++ app/laravel/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Git Hub Action CI/CD Test
                 </div>
                 <img src="" alt="">
                 <div class="links">
